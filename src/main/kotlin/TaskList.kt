2f80096005d1b60eabd1e1ab8a5eb49a8cef57b6
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-12-11 13:54:30 +0000

    Update TaskList.kt

--- src/main/kotlin/TaskList.kt
+++ src/main/kotlin/TaskList.kt
@@ -4,15 +4,15 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun TaskList(
-    listaItems: List<Task>, // Parámetro para la lista de tareas
-    onCheckedTask: (task: Task, checked: Boolean) -> Unit, // Función para cambiar el estado de la tarea
-    onCloseTask: (task: Task) -> Unit // Función para eliminar una tarea
+    listaItems: List<Task>,
+    onCheckedTask: (task: Task, checked: Boolean) -> Unit, 
+    onCloseTask: (task: Task) -> Unit 
 ) {
     LazyColumn {
         items(listaItems) { task ->
             TaskItem(
-                task = task, // Pasamos la tarea completa
-                onClose = { onCloseTask(task) }, // Pasamos la función onClose con la tarea correspondiente
+                task = task, 
+                onClose = { onCloseTask(task) }, 
             )
         }
     }
